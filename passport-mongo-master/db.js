55575b8a049bbb6965e7c647c0299b8e343d0d18
Bethany Wong <bethanywong@Bethanys-Air.fios-router.home>
2016-08-19 20:37:55 -0400

    login stuff that uses jade

---
+++ passport-mongo-master/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	'url' : 'mongodb://helpdesk2:password@ds025802.mlab.com:25802/schedulerdb'
+}
